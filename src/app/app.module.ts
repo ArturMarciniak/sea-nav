511b29c3818e178867189c47faacbca1c4d873cc
Artur Marciniak (Ext.) <artur.marciniak@FFMMBP-321.HOM-DE.SYZYGY.INTERN>
2024-02-05 06:19:36 +0100

    fix(app.module): Imports organised

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSelectModule } from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { ChartComponent } from './components/chart/chart.component';
 import { MapComponent } from './components/map/map.component';
 import { RoutePickerComponent } from './components/route-picker/route-picker.component';
-import { ChartComponent } from './components/chart/chart.component';
 
 @NgModule({
   declarations: [
